dd73b11d33c9fcc81c1be55c46ad0bc1d729be3c
rudrapratap7 <55956327+rudrapratap7@users.noreply.github.com>
2022-12-01 15:02:30 +0530

    Add files via upload

---
+++ terraform/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "Bastion" {
+
+  ami                    = "ami-0149b2da6ceec4bb0"
+  instance_type          = "t3.small"
+
+  key_name               = "abhi"
+  security_groups        = [aws_security_group.bastion-host134_SG.name]
+
+  tags = {
+    Sg  = "bastion"
+  }
+}
+
+resource "aws_instance" "Jenkins" {
+
+  ami                    = "ami-0149b2da6ceec4bb0"
+  instance_type          = "t3.small"
+  security_groups        = [aws_security_group.private-SG.name]
+  key_name               = "abhi"
+
+  tags = {
+    Sg  = "jenkins"
+}
+}
+resource "aws_instance" "App" {
+
+  ami                    = "ami-0149b2da6ceec4bb0"
+  instance_type          = "t3.small"
+  key_name               = "abhi"
+  security_groups        = [aws_security_group.Public-web_SG.name]
+
+
+  tags = {
+    Sg  = "App"
+  }
+}
---
+++ terraform/publicsg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "Public-web_SG" {
+  name        = "Public-web_SG"
+  description = "Allow port 80 form self ip"
+  vpc_id      = aws_vpc.abhi134.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "HTTP"
+    cidr_blocks      = ["49.43.42.187/32"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow http"
+  }
+}
