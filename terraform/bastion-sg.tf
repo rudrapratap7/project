395ed03a65e4fa0567638f7dc1c4a80475a23008
rudrapratap7 <55956327+rudrapratap7@users.noreply.github.com>
2022-12-02 13:07:38 +0530

    Add files via upload

---
+++ terraform/bastion-sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "bastion-host134_SG" {
+  name        = "bastion-host134_SG"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.abhi134.id
+
+  ingress {
+    description      = "ssh from bastion"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "ssh"
+    cidr_blocks      = ["49.43.42.187/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow ssh"
+  }
+}
---
+++ terraform/privatesg.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "private_SG" {
+
+ name        = "private_SG"
+  description = "Allow all inbound traffic withn vpc"
+  vpc_id      = aws_vpc.abhi134.id
+
+  ingress {
+    description      = "all traffic from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = [aws_vpc.abhi134.cidr_block]
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
