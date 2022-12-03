402bf7cdc6775f85443f9ea646e6bdd6839c0e69
rudrapratap7 <55956327+rudrapratap7@users.noreply.github.com>
2022-12-03 12:24:20 +0530

    Update bastion-sg.tf

--- terraform/bastion-sg.tf
+++ terraform/bastion-sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "bastion-host134_SG" {
     description      = "ssh from bastion"
     from_port        = 22
     to_port          = 22
-    protocol         = "ssh"
+    protocol         = "tcp"
     cidr_blocks      = ["49.43.42.187/32"]
   }
 
